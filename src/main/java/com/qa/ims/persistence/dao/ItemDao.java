b37dad6d8806927e4633a45bd54487f32abed8b0
henry-oliver-edwards <HOliver-Edwards@academytrainee.com>
2021-01-21 14:37:02 +0000

    IMS-5 implemented methods into the ItemDao for items to be created

---
+++ src/main/java/com/qa/ims/persistence/dao/ItemDao.java
@@ -0,0 +1,66 @@
+package com.qa.ims.persistence.dao;
+
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.DatabaseUtilities;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.*;
+import java.util.List;
+
+public class ItemDao implements IDomainDao<Item> {
+
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public Item readLatest() {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM items ORDER BY id DESC LIMIT 1")) {
+            resultSet.next();
+            return modelFromResultSet(resultSet);
+        } catch (Exception e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public Item create(Item item) {
+        try (Connection connection = DatabaseUtilities.getInstance().getConnection();
+             PreparedStatement statement = connection
+                     .prepareStatement("INSERT INTO items(name, price) VALUES (?, ?)")) {
+            statement.setString(1, item.getName());
+            statement.setDouble(2, item.getPrice());
+            statement.executeUpdate();
+            return readLatest();
+        } catch (Exception e) {
+            LOGGER.debug(e);
+            LOGGER.error(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public List<Item> readAll() {
+        return null;
+    }
+
+    @Override
+    public Item update(Item item) {
+        return null;
+    }
+
+    @Override
+    public int delete(long id) {
+        return 0;
+    }
+
+    @Override
+    public Item modelFromResultSet(ResultSet resultSet) throws SQLException {
+        Long id = resultSet.getLong("id");
+        String name = resultSet.getString("name");
+        Double price = resultSet.getDouble("price");
+        return new Item(id, name, price);
+    }
+}
